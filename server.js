4249cc1bf31427ad672ebfef273767fbf834c7e8
hammadali1805 <hammadalipbt18@gmail.com>
2025-12-24 22:29:42 +0530

    fixed CORS

--- server.js
+++ server.js
@@ -17,7 +17,16 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: [
+    'https://mestorefrontent.vercel.app',
+    'http://localhost:3000',
+    'http://localhost:5173'
+  ],
+  credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
